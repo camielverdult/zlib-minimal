93fca87bf3487c68da4091f31aff73cd9edb38a6
Camiel Verdult <cpg@verdult.com>
2022-11-22 15:38:54 +0100

    Use inflateInit2 with -MAX_WBITS
    See https://stackoverflow.com/questions/18700656/zlib-inflate-failing-with-3-z-data-error

--- zpipe.c
+++ zpipe.c
@@ -90,7 +90,7 @@ int inf(FILE *source, FILE *dest)
     strm.opaque = Z_NULL;
     strm.avail_in = 0;
     strm.next_in = Z_NULL;
-    ret = inflateInit(&strm);
+    ret = inflateInit2(&strm, -MAX_WBITS);
     if (ret != Z_OK)
         return ret;
 
